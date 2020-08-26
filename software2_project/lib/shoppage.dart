72e3109a79ec6c1e0ff351328e2d624101bbe45c
Mohamed Fares <mosaber7@ksu.edu>
2020-08-26 17:31:11 +0200

    shop page is succesfuly compiled

--- software2_project/lib/shoppage.dart
+++ software2_project/lib/shoppage.dart
@@ -6,55 +6,108 @@ class shoppage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: buildAppBar(),
-      body: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.all(0),
-            ),
-            Container(
-              height: 180,
-              width: 140,
-              child: Image.asset(
-                'assets/pexels-bùi-huy-1750045.jpg',
-              ),
-              decoration: BoxDecoration(
-                  color: Colors.pink[200],
-                  borderRadius: BorderRadius.circular(16)),
-            ),
-            Text("sneeker"),
-            Text('\$222'),
-            Container(
-              height: 180,
-              width: 140,
-              child: Image.asset(
-                'assets/pexels-melvin-buezo-2529148.jpg',
-              ),
-              decoration: BoxDecoration(
-                  color: Colors.pink[200],
-                  borderRadius: BorderRadius.circular(16)),
-            ),
-            Text("sneeker"),
-            Text('\$222'),
-            Container(
-              height: 180,
-              width: 140,
-              child: Image.asset(
-                'assets/pexels-jordan-hyde-1032110.jpg',
-              ),
-              decoration: BoxDecoration(
-                  color: Colors.pink[200],
-                  borderRadius: BorderRadius.circular(16)),
-            ),
-            Text("sneeker"),
-            Text('\$222'),
-          ],
-        ),
-      ),
+      body: GridView.count(
+          crossAxisCount: 2,
+          childAspectRatio: 0.75,
+          children: List.generate(100, (index) {
+            return products[index % 6];
+          })),
     );
   }
 }
+
+List<Column> products = [
+  Column(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: <Widget>[
+      Container(
+        margin: EdgeInsets.fromLTRB(10, 0, 10, 10),
+        height: 180,
+        width: 160,
+        child: Image.asset('assets/bag_1.png'),
+        decoration: BoxDecoration(
+            color: Colors.blue[600], borderRadius: BorderRadius.circular(16)),
+      ),
+      Text("sneeker"),
+      Text('22'),
+    ],
+  ),
+  Column(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: <Widget>[
+      Container(
+        margin: EdgeInsets.fromLTRB(10, 0, 10, 10),
+        height: 180,
+        width: 160,
+        child: Image.asset('assets/bag_2.png'),
+        decoration: BoxDecoration(
+            color: Colors.brown[400], borderRadius: BorderRadius.circular(16)),
+      ),
+      Text("sneeker"),
+      Text('212'),
+    ],
+  ),
+  Column(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: <Widget>[
+      Container(
+        margin: EdgeInsets.fromLTRB(10, 0, 10, 10),
+        height: 180,
+        width: 160,
+        child: Image.asset('assets/bag_3.png'),
+        decoration: BoxDecoration(
+            color: Colors.grey[600], borderRadius: BorderRadius.circular(16)),
+      ),
+      Text("sneeker"),
+      Text('22'),
+    ],
+  ),
+  Column(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: <Widget>[
+      Container(
+        margin: EdgeInsets.fromLTRB(10, 0, 10, 10),
+        height: 180,
+        width: 160,
+        child: Image.asset('assets/bag_4.png'),
+        decoration: BoxDecoration(
+            color: Colors.brown[300], borderRadius: BorderRadius.circular(16)),
+      ),
+      Text("sneeker"),
+      Text('22'),
+    ],
+  ),
+  Column(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: <Widget>[
+      Container(
+        margin: EdgeInsets.fromLTRB(10, 0, 10, 10),
+        height: 180,
+        width: 160,
+        child: Image.asset('assets/bag_5.png'),
+        decoration: BoxDecoration(
+            color: Colors.red[500], borderRadius: BorderRadius.circular(16)),
+      ),
+      Text("sneeker"),
+      Text('22'),
+    ],
+  ),
+  Column(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: <Widget>[
+      Container(
+        margin: EdgeInsets.fromLTRB(10, 0, 10, 10),
+        height: 180,
+        width: 160,
+        child: Image.asset('assets/bag_6.png'),
+        decoration: BoxDecoration(
+            color: Colors.grey[700], borderRadius: BorderRadius.circular(16)),
+      ),
+      Text("sneeker"),
+      Text('22'),
+    ],
+  ),
+];
 
 AppBar buildAppBar() {
   return AppBar(
